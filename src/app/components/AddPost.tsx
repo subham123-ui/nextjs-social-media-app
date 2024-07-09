0be35ee93c4624245ecb11c39caaed945b5666f3
subham123-ui <suvam.rockstar@gmail.com>
2024-07-09 23:58:57 +0530

    frnt

--- src/app/components/AddPost.tsx
+++ src/app/components/AddPost.tsx
@@ -1,11 +1,54 @@
-
+import Image from "next/image";
 
 const AddPost = () => {
   return (
-    <div>
-      AddPost
+    <div className="p-4 bg-white shadow-md rounded-lg flex gap-4 justify-between text-sm">
+      {/* AVATAR */}
+      <Image
+        src="https://images.pexels.com/photos/3721535/pexels-photo-3721535.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+        alt=""
+        width={48}
+        height={48}
+        className="w-12 h-12 object-cover rounded-full"
+      />
+      {/* POST */}
+      <div className=" flex-1">
+        {/* TEXT INPUT */}
+        <div className="flex gap-4">
+          <textarea
+            placeholder="What's on your mind?"
+            className="bg-slate-100 flex-1 rounded-lg p-2"
+          ></textarea>
+          <Image
+            src="/emoji.png"
+            alt=""
+            width={20}
+            height={20}
+            className="w-5 h-5 cursor-pointer self-end"
+          />
+        </div>
+        {/* POST OPTIONS */}
+        <div className="flex items-center gap-4 mt-4 text-gray-400 flex-wrap ">
+          <div className="flex items-center gap-2 cursor-pointer">
+            <Image src="/addimage.png" alt="" width={20} height={20} />
+            Photo
+          </div>
+          <div className=" flex items-center gap-2 cursor-pointer">
+            <Image src="/addVideo.png" alt="" width={20} height={20} />
+            Video
+          </div>
+          <div className=" flex items-center gap-2 cursor-pointer">
+            <Image src="/poll.png" alt="" width={20} height={20} />
+            Poll
+          </div>
+          <div className=" flex items-center gap-2 cursor-pointer">
+            <Image src="/addevent.png" alt="" width={20} height={20} />
+            Event
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default AddPost;
--- src/app/components/Stories.tsx
+++ src/app/components/Stories.tsx
@@ -65,6 +65,66 @@ const Stories = () => {
           />
           <span className="font-medium">GOGO</span>
         </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/20988789/pexels-photo-20988789/free-photo-of-cup-of-coffee-on-a-wooden-table.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">GOGO</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/20988789/pexels-photo-20988789/free-photo-of-cup-of-coffee-on-a-wooden-table.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">GOGO</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/20988789/pexels-photo-20988789/free-photo-of-cup-of-coffee-on-a-wooden-table.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">GOGO</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/20988789/pexels-photo-20988789/free-photo-of-cup-of-coffee-on-a-wooden-table.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">GOGO</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/20988789/pexels-photo-20988789/free-photo-of-cup-of-coffee-on-a-wooden-table.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">GOGO</span>
+        </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer">
+          <Image
+            src="https://images.pexels.com/photos/20988789/pexels-photo-20988789/free-photo-of-cup-of-coffee-on-a-wooden-table.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            alt=""
+            width={80}
+            height={80}
+            className="w-20 h-20 rounded-full ring-2"
+          />
+          <span className="font-medium">GOGO</span>
+        </div>
       </div>
     </div>
   );
