5d235a5a3399900a543126cd1c0e93a4c4b7a7e9
subham123-ui <suvam.rockstar@gmail.com>
2024-07-11 23:50:01 +0530

    frnt

--- src/app/components/Stories.tsx
+++ src/app/components/Stories.tsx
@@ -1,130 +1,41 @@
+import prisma from "@/lib/client";
+import { auth } from "@clerk/nextjs/server";
 import Image from "next/image";
+import StoryList from "./StoryList";
 
-const Stories = () => {
+const Stories = async () => {
+  const { userId: currentUserId } = auth();
+
+  if (!currentUserId) return null;
+
+  const stories = await prisma.story.findMany({
+    where: {
+      expiresAt: {
+        gt: new Date(),
+      },
+      OR: [
+        {
+          user: {
+            followers: {
+              some: {
+                followerId: currentUserId,
+              },
+            },
+          },
+        },
+        {
+          userId: currentUserId,
+        },
+      ],
+    },
+    include: {
+      user: true,
+    },
+  });
   return (
     <div className="p-4 bg-white rounded-lg shadow-md overflow-scroll text-xs scrollbar-hide">
-      <div className=" flex gap-8 w-max">
-        {/* STORIES */}
-        <div className="flex flex-col items-center gap-2 cursor-pointer">
-          <Image
-            src="https://images.pexels.com/photos/20988789/pexels-photo-20988789/free-photo-of-cup-of-coffee-on-a-wooden-table.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-            alt=""
-            width={80}
-            height={80}
-            className="w-20 h-20 rounded-full ring-2"
-          />
-          <span className="font-medium">GOGO</span>
-        </div>
-        <div className="flex flex-col items-center gap-2 cursor-pointer">
-          <Image
-            src="https://images.pexels.com/photos/20988789/pexels-photo-20988789/free-photo-of-cup-of-coffee-on-a-wooden-table.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-            alt=""
-            width={80}
-            height={80}
-            className="w-20 h-20 rounded-full ring-2"
-          />
-          <span className="font-medium">GOGO</span>
-        </div>
-        <div className="flex flex-col items-center gap-2 cursor-pointer">
-          <Image
-            src="https://images.pexels.com/photos/20988789/pexels-photo-20988789/free-photo-of-cup-of-coffee-on-a-wooden-table.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-            alt=""
-            width={80}
-            height={80}
-            className="w-20 h-20 rounded-full ring-2"
-          />
-          <span className="font-medium">GOGO</span>
-        </div>
-        <div className="flex flex-col items-center gap-2 cursor-pointer">
-          <Image
-            src="https://images.pexels.com/photos/20988789/pexels-photo-20988789/free-photo-of-cup-of-coffee-on-a-wooden-table.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-            alt=""
-            width={80}
-            height={80}
-            className="w-20 h-20 rounded-full ring-2"
-          />
-          <span className="font-medium">GOGO</span>
-        </div>
-        <div className="flex flex-col items-center gap-2 cursor-pointer">
-          <Image
-            src="https://images.pexels.com/photos/20988789/pexels-photo-20988789/free-photo-of-cup-of-coffee-on-a-wooden-table.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-            alt=""
-            width={80}
-            height={80}
-            className="w-20 h-20 rounded-full ring-2"
-          />
-          <span className="font-medium">GOGO</span>
-        </div>
-        <div className="flex flex-col items-center gap-2 cursor-pointer">
-          <Image
-            src="https://images.pexels.com/photos/20988789/pexels-photo-20988789/free-photo-of-cup-of-coffee-on-a-wooden-table.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-            alt=""
-            width={80}
-            height={80}
-            className="w-20 h-20 rounded-full ring-2"
-          />
-          <span className="font-medium">GOGO</span>
-        </div>
-        <div className="flex flex-col items-center gap-2 cursor-pointer">
-          <Image
-            src="https://images.pexels.com/photos/20988789/pexels-photo-20988789/free-photo-of-cup-of-coffee-on-a-wooden-table.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-            alt=""
-            width={80}
-            height={80}
-            className="w-20 h-20 rounded-full ring-2"
-          />
-          <span className="font-medium">GOGO</span>
-        </div>
-        <div className="flex flex-col items-center gap-2 cursor-pointer">
-          <Image
-            src="https://images.pexels.com/photos/20988789/pexels-photo-20988789/free-photo-of-cup-of-coffee-on-a-wooden-table.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-            alt=""
-            width={80}
-            height={80}
-            className="w-20 h-20 rounded-full ring-2"
-          />
-          <span className="font-medium">GOGO</span>
-        </div>
-        <div className="flex flex-col items-center gap-2 cursor-pointer">
-          <Image
-            src="https://images.pexels.com/photos/20988789/pexels-photo-20988789/free-photo-of-cup-of-coffee-on-a-wooden-table.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-            alt=""
-            width={80}
-            height={80}
-            className="w-20 h-20 rounded-full ring-2"
-          />
-          <span className="font-medium">GOGO</span>
-        </div>
-        <div className="flex flex-col items-center gap-2 cursor-pointer">
-          <Image
-            src="https://images.pexels.com/photos/20988789/pexels-photo-20988789/free-photo-of-cup-of-coffee-on-a-wooden-table.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-            alt=""
-            width={80}
-            height={80}
-            className="w-20 h-20 rounded-full ring-2"
-          />
-          <span className="font-medium">GOGO</span>
-        </div>
-        <div className="flex flex-col items-center gap-2 cursor-pointer">
-          <Image
-            src="https://images.pexels.com/photos/20988789/pexels-photo-20988789/free-photo-of-cup-of-coffee-on-a-wooden-table.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-            alt=""
-            width={80}
-            height={80}
-            className="w-20 h-20 rounded-full ring-2"
-          />
-          <span className="font-medium">GOGO</span>
-        </div>
-        <div className="flex flex-col items-center gap-2 cursor-pointer">
-          <Image
-            src="https://images.pexels.com/photos/20988789/pexels-photo-20988789/free-photo-of-cup-of-coffee-on-a-wooden-table.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
-            alt=""
-            width={80}
-            height={80}
-            className="w-20 h-20 rounded-full ring-2"
-          />
-          <span className="font-medium">GOGO</span>
-        </div>
+      <div className="flex gap-8 w-max">
+        <StoryList stories={stories} userId={currentUserId}/>
       </div>
     </div>
   );
