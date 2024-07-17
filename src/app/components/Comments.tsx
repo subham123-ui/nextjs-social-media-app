757046ec6be82b08d3c8822ad96afb874a169d1b
subham123-ui <suvam.rockstar@gmail.com>
2024-07-17 23:51:41 +0530

    frnt

---
+++ src/app/components/Comments.tsx
@@ -0,0 +1,36 @@
+import Image from "next/image";
+
+const Comments = () => {
+  return (
+    <div className="">
+      {/* WRITE */}
+      <div className="flex items-center gap-4 ">
+        <Image
+          src="https://images.pexels.com/photos/24244036/pexels-photo-24244036/free-photo-of-road-trip.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+          height={32}
+          width={32}
+          className="w-8 h-8 rounded-full"
+        />
+        <div className="flex items-center justify-between bg-slate-100 rounded-xl text-sm px-6 py-2 w-full">
+          <input
+            type="text"
+            placeholder="Write a comment..."
+            className="bg-transparent outline-none flex-1"
+          />
+          <Image
+            src="/emoji.png"
+            alt=""
+            height={16}
+            width={16}
+            className="cursor-pointer"
+          />
+        </div>
+      </div>
+      {/* COMMENTS */}
+      <div className=""></div>
+    </div>
+  );
+};
+
+export default Comments;
--- src/app/components/Post.tsx
+++ src/app/components/Post.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Comments from "./Comments";
 
 const Post = () => {
   return (
@@ -7,8 +8,8 @@ const Post = () => {
       <div className="flex  items-center justify-between">
         <div className="flex items-center gap-4">
           <Image
-            src="https://images.pexels.com/photos/17685561/pexels-photo-17685561/free-photo-of-a-surfer-is-in-the-ocean-at-sunset.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load"
             alt=""
+            src="https://images.pexels.com/photos/24425289/pexels-photo-24425289/free-photo-of-a-cake-with-fruit-and-a-plate-of-food.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
             width={40}
             height={40}
             className="w-10 h-10 rounded-full"
@@ -18,9 +19,63 @@ const Post = () => {
         <Image src="/more.png" alt="" width={20} height={20} className="" />
       </div>
       {/* DESC */}
-      <div className=""></div>
+      <div className="flex  flex-col gap-4">
+        <div className="w-full min-h-96 relative">
+          <Image
+            alt=""
+            src="https://images.pexels.com/photos/17499608/pexels-photo-17499608/free-photo-of-view-of-the-castle-on-bled-island-on-lake-bled-in-slovenia.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+            fill
+            className="object-cover rounded-md "
+          />
+        </div>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi labore
+          eos a consequatur non et itaque, beatae dolorum saepe voluptatem
+          obcaecati facilis illo veniam consectetur similique illum ipsa
+          officiis eius.
+        </p>
+      </div>
       {/* INTERACTION */}
-      <div className=""></div>
+      <div className="flex items-center justify-between text-sm my-4">
+        <div className="flex gap-8">
+          <div className="flex items-center gap-2 bg-slate-50 p-2 rounded-xl">
+            <Image
+              src="/like.png"
+              alt=""
+              width={16}
+              height={16}
+              className="cursor-pointer"
+            />
+            <span className="text-gray-300">|</span>
+            <span className="text-gray-500">1,234 <span  className="hidden md:inline">Likes</span></span>
+          </div>
+          <div className="flex items-center gap-2 bg-slate-50 p-2 rounded-xl">
+            <Image
+              src="/comment.png"
+              alt=""
+              width={16}
+              height={16}
+              className="cursor-pointer"
+            />
+            <span className="text-gray-300">|</span>
+            <span className="text-gray-500">234 <span  className="hidden md:inline">Comments</span></span>
+          </div>
+        </div>
+        <div className="">
+        <div className="flex items-center gap-2 bg-slate-50 p-2 rounded-xl">
+            <Image
+              src="/share.png"
+              alt=""
+              width={16}
+              height={16}
+              className="cursor-pointer"
+            />
+            <span className="text-gray-300">|</span>
+            <span className="text-gray-500">234 <span  className="hidden md:inline">Shares</span></span>
+          </div>
+        </div>
+      </div>
+      <Comments/>
     </div>
   );
 };
